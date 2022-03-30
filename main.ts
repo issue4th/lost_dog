a85273316d982242a5b7758816cdcd50621e45e4
issue4th <44305925+issue4th@users.noreply.github.com>
2022-03-30 17:33:35 +0100

    made chests work and put new level in

--- main.ts
+++ main.ts
@@ -1,26 +1,56 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile24`, function (sprite, location) {
+    if (diamond_chest_open) {
+    	
+    } else {
+        diamond_chest_open = sprites.create(img`
+            . b b b b b b b b b b b b b b . 
+            b 6 9 9 9 9 9 9 9 9 9 9 9 9 9 b 
+            b 6 9 9 9 9 9 9 9 9 9 9 9 9 6 b 
+            b 6 6 9 9 9 9 9 9 9 9 9 9 6 6 b 
+            b b b b b b b d d b b b b b b b 
+            . b b b b b b c c b b b b b b . 
+            b c c 8 8 c b c c b c c c c c b 
+            b 8 c 8 8 c 8 b b 8 c 8 9 8 c b 
+            b 8 8 9 9 8 9 9 8 8 9 f 8 9 8 b 
+            b 9 f 9 8 f 8 9 f 9 8 8 8 8 f b 
+            b b b b b b b b b b b b b b b b 
+            b 6 6 6 6 6 6 6 6 6 6 6 6 6 6 b 
+            b 6 6 6 6 6 6 6 6 6 6 6 6 6 6 b 
+            b c 6 6 6 6 6 6 6 6 6 6 6 6 c b 
+            b b b b b b b b b b b b b b b b 
+            . b b . . . . . . . . . . b b . 
+            `, SpriteKind.Player)
+        tiles.placeOnRandomTile(diamond_chest_open, assets.tile`myTile24`)
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
-    chest_open = sprites.create(img`
-        . b b b b b b b b b b b b b b . 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
-        b b b b b b b d d b b b b b b b 
-        . b b b b b b c c b b b b b b . 
-        b c c 5 5 c b c c b c c c c c b 
-        b 5 c 5 5 c 4 b b 5 c 5 4 5 c b 
-        b 4 5 4 4 5 4 4 5 4 4 f 5 4 5 b 
-        b 4 f 4 4 f 4 4 f 4 5 5 5 4 f b 
-        b b b b b b b b b b b b b b b b 
-        b e e e e e e e e e e e e e e b 
-        b e e e e e e e e e e e e e e b 
-        b c e e e e e e e e e e e e c b 
-        b b b b b b b b b b b b b b b b 
-        . b b . . . . . . . . . . b b . 
-        `, SpriteKind.Player)
-    tiles.placeOnRandomTile(chest_open, assets.tile`myTile2`)
+    if (chest_open) {
+    	
+    } else {
+        chest_open = sprites.create(img`
+            . b b b b b b b b b b b b b b . 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+            b b b b b b b d d b b b b b b b 
+            . b b b b b b c c b b b b b b . 
+            b c c 5 5 c b c c b c c c c c b 
+            b 5 c 5 5 c 4 b b 5 c 5 4 5 c b 
+            b 4 5 4 4 5 4 4 5 4 4 f 5 4 5 b 
+            b 4 f 4 4 f 4 4 f 4 5 5 5 4 f b 
+            b b b b b b b b b b b b b b b b 
+            b e e e e e e e e e e e e e e b 
+            b e e e e e e e e e e e e e e b 
+            b c e e e e e e e e e e e e c b 
+            b b b b b b b b b b b b b b b b 
+            . b b . . . . . . . . . . b b . 
+            `, SpriteKind.Player)
+        tiles.placeOnRandomTile(chest_open, assets.tile`myTile2`)
+    }
 })
 let chest_open: Sprite = null
-tiles.loadMap(tiles.createMap(tilemap`level1`))
+let diamond_chest_open: Sprite = null
+tiles.loadMap(tiles.createMap(tilemap`level2`))
 let dog = sprites.create(img`
     e e e . . . . e e e . . . . 
     c d d c . . c d d c . . . . 
